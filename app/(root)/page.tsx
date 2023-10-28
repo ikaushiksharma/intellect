a52eb59a908562f58c74ec01ea5eaf8ef15d7b35
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2023-10-28 14:53:55 +0530

    folder structure ready

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-emerald-500 text-white">Delete</Button>
+    </div>
+  );
+}
